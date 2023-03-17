42a4982ad7ac5ec83be1e8b4bd8883350918af3c
DavidUMC <david.perez@upmycard.com>
2023-03-17 11:51:17 +0100

    first steps using twig templates

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,15 +2,30 @@
 
 namespace David\Blogpro\Controller;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class PostsController
 {
+    private $loader;
+    protected $twig;
+    
+    public function __construct()
+    {
+        $this->loader = new FilesystemLoader('src/templates');
+
+        $this->twig = new Environment($this->loader);
+    }
+
     public function index()
     {
-        echo "tous les articles du blog";
+        $this->twig->display('posts/index.html.twig');
+        //echo "tous les articles du blog";
     }
 
     public function show($id)
     {
-        echo "Je suis l'article $id";
+        $this->twig->display('posts/show.html.twig', compact('id'));
+        //echo "Je suis l'article $id";
     }
 }
