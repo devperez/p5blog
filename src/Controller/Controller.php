48a97e28590fb1a49d12d0f3a2352dc3a30a6731
DavidUMC <david.perez@upmycard.com>
2023-03-24 12:37:29 +0100

    started to implement database connection in order to display the index page and an article page

---
+++ src/Controller/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace David\Blogpro\Controller;
+
+use David\Blogpro\Database\DBConnection;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+abstract class Controller
+{
+    private $loader;
+    protected $twig;
+    protected $db;
+    
+    public function __construct(DBConnection $db)
+    {
+        $this->loader = new FilesystemLoader(ROOT . '/blogpro/templates');
+        $this->twig = new Environment($this->loader);
+
+        $this->db = $db;
+    }
+}
