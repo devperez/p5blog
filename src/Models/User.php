823319e1688978964f5dd5b85faa5ed0d3b6a9be
DavidUMC <david.perez@upmycard.com>
2023-04-14 19:59:59 +0200

    changed the name of a method to respect the camelCase convention

--- src/Models/User.php
+++ src/Models/User.php
@@ -13,14 +13,14 @@ class User extends Model
 
     public function create($username, $email, $password): PDOStatement
     {
-        $user = $this->db->getPDO()->prepare("INSERT INTO user(username, email, password) VALUES(?, ?, ?)");
+        $user = $this->db->getPdo()->prepare("INSERT INTO user(username, email, password) VALUES(?, ?, ?)");
         $user->execute([$username, $email, $password]);
         return $user;
     }
 
     public function getOne($email, $password): array
     {
-        $user = $this->db->getPDO()->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
+        $user = $this->db->getPdo()->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
         $user->execute([$email, $password]);
         $user = $user->fetch();
         return $user;
