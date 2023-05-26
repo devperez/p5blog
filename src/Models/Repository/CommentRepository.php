3af1877695f61f8420343563cfc42019d7f04d7b
DavidUMC <david.perez@upmycard.com>
2023-05-26 17:38:46 +0200

    started documenting the code

--- src/Models/Repository/CommentRepository.php
+++ src/Models/Repository/CommentRepository.php
@@ -7,20 +7,34 @@ use PDOStatement;
 
 class CommentRepository extends AbstractRepository
 {
-    public function create($commentContent, $userId, $postId): PDOStatement
+    /***
+     * @param string $commentContent
+     * @param integer $userId
+     * @param integer $postId
+     * @return pdoStatement
+     */
+    public function create(string $commentContent, int $userId, int $postId): PDOStatement
     {
         $comment = new Comment($this->db);
         $comment = $comment->create($commentContent, $userId, $postId);
         return $comment;
     }
 
+    /***
+     * @return array
+     */
+
     public function index(): array
     {
         $comment = new Comment($this->db);
         $comments = $comment->getAll();
-        
         return $comments;
     }
+
+    /***
+     * @param integer $id
+     * @return array
+     */
 
     public function getOneById(int $id): array
     {
@@ -29,11 +43,21 @@ class CommentRepository extends AbstractRepository
         return $comment;
     }
 
+    /***
+     * @param integer $commentId
+     * @return void
+     */
+
     public function publish(int $commentId): void
     {
         $comment = new Comment($this->db);
         $comment = $comment->updateComment($commentId);
     }
+
+    /***
+     * @param integer $postId
+     * @return array
+     */
 
     public function getCommentFromPost(int $postId): array
     {
@@ -42,6 +66,11 @@ class CommentRepository extends AbstractRepository
         return $comments;
     }
 
+    /***
+     * @param integer $commentId
+     * @return void
+     */
+    
     public function delete(int $commentId): void
     {
         $comment = new Comment($this->db);
