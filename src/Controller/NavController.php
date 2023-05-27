1cddb652c749e2e8aed424833de9822307ee061e
DavidUMC <david.perez@upmycard.com>
2023-05-27 14:44:50 +0200

    created a distinct class for email sending and used env variables

--- src/Controller/NavController.php
+++ src/Controller/NavController.php
@@ -2,12 +2,11 @@
 
 namespace David\Blogpro\Controller;
 
+use David\Blogpro\Mail\Mail;
 use David\Blogpro\Models\Repository\CommentRepository;
 use David\Blogpro\Models\Repository\PostRepository;
 use David\Blogpro\Models\Repository\UserRepository;
 use David\Blogpro\Session\Session;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
 
 class NavController extends Controller
 {
@@ -63,29 +62,15 @@ class NavController extends Controller
         $email = $_POST['email'];
         $message = $_POST['message'];
         
-        if ($name == '' || $email === '' || $message == '') {
+        if (trim($name) === '' || trim($email) === '' || trim($message) === '') {
             $error = 'Merci de bien vouloir remplir tous les champs du formulaire.';
             $this->twig->display('homepage.html.twig', ['error' => $error]);
         } else {
-            $mail = new PHPMailer(true);
-            $message = wordwrap($message, 70);
-            $success = 'Mail envoyé avec succès !';
-            try {
-                $mail->isSMTP();
-                $mail->Host = 'localhost';
-                $mail->Port = 1025;
-
-                $mail->setFrom($email, $name);
-                $mail->addAddress('contact@blogpro.fr');
-
-                $mail->isHTML(true);
-                $mail->Subject = "Demande de renseignements";
-                $mail->Body = $message;
-
-                $mail->send();
+            $mail = new Mail();
+            $mail = $mail->send($name, $email, $message);
+            if ($mail) {
+                $success = 'Mail envoyé avec succès !';
                 $this->twig->display('homepage.html.twig', ['success' => $success]);
-            } catch (Exception $e) {
-                echo "Message non envoyé : {$mail->ErrorInfo}";
             }
         }
     }
---
+++ src/Mail/Mail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace David\Blogpro\Mail;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mail
+{
+    /***
+     * @param string $name
+     * @param string $email
+     * @param string $message
+     * @return boolean true if mail is sent
+     */
+    public function send(string $name, string $email, string $message): bool
+    {
+        $mail = new PHPMailer(true);
+        $message = wordwrap($message, 70);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host = $_ENV['HOST'];
+            $mail->Port = $_ENV['PORT'];
+
+            $mail->setFrom($email, $name);
+            $mail->addAddress($_ENV['ADDRESS']);
+
+            $mail->isHTML(true);
+            $mail->Subject = $_ENV['SUBJECT'];
+            $mail->Body = $message;
+
+            $mail->send();
+            return true;
+        } catch (Exception $e) {
+            echo "Message non envoyé : {$mail->ErrorInfo}";
+        }
+    }
+}
