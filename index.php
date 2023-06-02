229aa4843fb9ddf0d0e145caf440bd942e1b53c6
DavidUMC <david.perez@upmycard.com>
2023-06-02 09:48:04 +0200

    a few changes on the index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+
 
 use David\Blogpro\Router\Router;
 use Symfony\Component\Dotenv\Dotenv;
 
-//constant aiming to the project root folder
+// constant aiming to the project root folder
 define('ROOT', dirname(__DIR__));
 
 require 'vendor/autoload.php';
@@ -15,45 +14,47 @@ $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
 $router = new Router($_GET['url']);
 
+if(isset($_GET['url']))
+{
+    $router->get('/', "Nav::homepage");
 
-$router->get('/', "Nav::homepage");
+    $router->get('/posts', "Nav::index");
 
-$router->get('/posts', "Nav::index");
+    $router->get('/posts/:id', "Nav::show");
 
-$router->get('/posts/:id', "Nav::show");
+    $router->get('/admin', "Nav::admin");
 
-$router->get('/admin', "Nav::admin");
+    $router->post('/sendMail', "Nav::mail");
 
-$router->post('/sendMail', "Nav::mail");
+    $router->post('/signup', "Admin::signup");
 
-$router->post('/signup', "Admin::signup");
+    $router->post('/signin', "Admin::signin");
 
-$router->post('/signin', "Admin::signin");
+    $router->get('/writePost', "Admin::write");
 
-$router->get('/writePost', "Admin::write");
+    $router->get('/indexAdmin', "Admin::index");
 
-$router->get('/indexAdmin', "Admin::index");
+    $router->post('/publishPost', "Admin::publish");
 
-$router->post('/publishPost', "Admin::publish");
+    $router->get('/editPost/:id', "Admin::edit");
 
-$router->get('/editPost/:id', "Admin::edit");
+    $router->post('/editPost/:id', "Admin::editPost");
 
-$router->post('/editPost/:id', "Admin::editPost");
+    $router->get('/deletePost/:id', "Admin::deletePost");
 
-$router->get('/deletePost/:id', "Admin::deletePost");
+    $router->get('readPost/:id', "Admin::readPost");
 
-$router->get('readPost/:id', "Admin::readPost");
+    $router->get('logout', "Admin::logout");
 
-$router->get('logout', "Admin::logout");
+    $router->post('/comment', "Admin::comment");
 
-$router->post('/comment', "Admin::comment");
+    $router->get('commentIndex', "Admin::commentIndex");
 
-$router->get('commentIndex', "Admin::commentIndex");
+    $router->get('readComment/:id', "Admin::readComment");
 
-$router->get('readComment/:id', "Admin::readComment");
+    $router->get('publishComment/:id', "Admin::publishComment");
 
-$router->get('publishComment/:id', "Admin::publishComment");
+    $router->get('deleteComment/:id', "Admin::deleteComment");
 
-$router->get('deleteComment/:id', "Admin::deleteComment");
-
-$router->run();
+    $router->run();
+}
