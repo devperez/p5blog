a38a7084e75ae3ffd503a8c4e0043ba3a904d3e4
DavidUMC <david.perez@upmycard.com>
2023-06-02 18:13:05 +0200

    few changes following codacy on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector("#sidebarToggle");
     if (sidebarToggle) {
         sidebarToggle.addEventListener("click", (event) => {
             event.preventDefault();
             document.body.classList.toggle("sb-sidenav-toggled");
-            localStorage.setItem("sb|sidebar-toggle", document.body.classList.contains('sb-sidenav-toggled'));
+            localStorage.setItem("sb|sidebar-toggle", document.body.classList.contains("sb-sidenav-toggled"));
         });
     }
 
